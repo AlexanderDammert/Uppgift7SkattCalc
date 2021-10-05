092712dffbd8d98cadd20626c258d854607e3c63
AlexanderDammert <alfawolfbusiness@gmail.com>
2021-10-05 00:40:43 -0800

    Calc done!

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var taxRate = 0;
+var quantity = 0;
+var unitPrice = 0;
+var exVAT = 0;
+var kmPieces = 0;
+var travelEx = 0;
+var tax = 0;
+var result = 0;
+//HTML VARS
+
+//helper vars
+
+
+
+
+function calculate(){
+    console.log(result)
+    console.log("started calc...")
+    //GET HTML
+    var taxRate = document.getElementById("taxRate");
+    var quantity = document.getElementById("quantity");
+    var unitPrice = document.getElementById("unitPrice");
+    var exVAT = document.getElementById("exVAT");
+    var kmPieces = document.getElementById("kmPieces");
+    var mealAllowance = document.getElementById("mealAllowance");
+    var travelExpenses = document.getElementById("travelExpenses");
+
+    //init
+    taxRate.value = taxRate.value / 100;
+    tax = taxRate.value
+
+    //parsing
+    exVAT = parseInt(exVAT.value)
+    kmPieces = parseInt(kmPieces.value)
+    travelExpenses = parseInt(travelExpenses.value)
+    mealAllowance = parseInt(mealAllowance.value)
+    //start calculation.
+
+    //rest
+    result = quantity.value * unitPrice.value;
+    result = result + exVAT;
+    result = result + kmPieces;
+    result = result + travelExpenses;
+    result = result + mealAllowance;
+
+    //tax
+    tax = tax * result
+
+    result = result - tax;
+
+    document.getElementById("result").innerHTML = "TOTAL IS : " +result;
+    document.getElementById("result2").innerHTML = "TAX PAID : " +tax;
+    console.log(result)
+}
